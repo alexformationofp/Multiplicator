b553efe25da0834f00c458cff3c461027d91858c
JeromeDias <JeromeDias@users.noreply.github.com>
2019-11-14 16:47:22 +0100

    modif

--- table.php
+++ table.php
@@ -15,6 +15,7 @@ $_SESSION['partie'] = 0;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="scss/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,800&display=swap" rel="stylesheet">
     <title>Multiplicator</title>
 </head>
 
