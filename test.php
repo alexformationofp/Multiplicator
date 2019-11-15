63ba3f4899afa669fb729f6fe7cc850ce776db10
alexformationofp <55798552+alexformationofp@users.noreply.github.com>
2019-11-15 09:16:55 +0100

    changement

--- test.php
+++ test.php
@@ -9,52 +9,57 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="scss/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister&display=swap" rel="stylesheet">
     <title>Multiplicator</title>
 </head>
 
 <body>
-    <div class="container">
-        <h1>Multiplicator</h1>
 
-        <h2>Révisez la table de <?php $nombre = $_SESSION['nombre'];echo $nombre; ?></h2>
-
-        <?php
-        if ($_SESSION['partie'] < 9) {
-            if (isset($_SESSION['mult']) && isset($_POST['essai'])) {
-                // test si c'est bon ou pas
-                $essai = $_POST['essai'];
-                $mult = $_SESSION['mult'];
-                $score = $_SESSION['score'];
-                $partie = $_SESSION['partie'];
-                if ($essai == $nombre * $mult) {
-                    echo '<p>Gagné</p!>';
-                    $score++;
-                    $partie++;
+    <h1>Multiplicator</h1>
+    <h2>Révisez la table de <?php $nombre = $_SESSION['nombre'];
+                            echo $nombre; ?></h2>
+    <div class="container relative">
+        <div class="choixTable">
+            <?php
+            if ($_SESSION['partie'] < 9) {
+                if (isset($_SESSION['mult']) && isset($_POST['essai'])) {
+                    // test si c'est bon ou pas
+                    $essai = $_POST['essai'];
+                    $mult = $_SESSION['mult'];
+                    $score = $_SESSION['score'];
+                    $partie = $_SESSION['partie'];
+                    if ($essai == $nombre * $mult) {
+                        echo '<p class="message">Gagné !</p>';
+                        $score++;
+                        $partie++;
+                    } else {
+                        echo '<p class="message">Loupé, </br>'.$mult.' X '.$nombre.' = ' . $nombre * $mult . '</p>';
+                        $partie++;
+                    }
+                    $_SESSION['mult'] = rand(1, 15);
+                    $_SESSION['score'] = $score;
+                    $_SESSION['partie'] = $partie;
+                    $mult = $_SESSION['mult'];
+                    echo '<p class="test">' . $mult . ' X ' . $nombre . '</br>=</p><form class="formulaire" action="test.php" method="post"><input class="tentative" type="text" size="3" name="essai" autofocus><input type="submit" class="valider" value="Valider"></form>';
                 } else {
-                    echo '<p>Loupé, c\'est ' . $nombre * $mult . '</p>';
-                    $partie++;
+                    $_SESSION['mult'] = rand(1, 15);
+                    $mult = $_SESSION['mult'];
+                    echo '<p class="test">' . $mult . ' X ' . $nombre . '</br>=</p><form class="formulaire" action="test.php" method="post"><input class="tentative" type="text" size="3" name="essai" autofocus><input type="submit" class="valider" value="Valider"></form>';
                 }
-                $_SESSION['mult'] = rand(1, 15);
-                $_SESSION['score'] = $score;
-                $_SESSION['partie'] = $partie;
-                $mult = $_SESSION['mult'];
-                echo '<p>Combien vaut ' . $mult . ' multiplié par ' . $nombre . ' ?</p><form action="test.php" method="post"><input type="text" name="essai" autofocus><input type="submit" value="Envoyer"></form>';
+                echo '<p class="score">Score : ' . $_SESSION['score'] . ' sur ' . $_SESSION['partie'] . '</p>';
             } else {
-                $_SESSION['mult'] = rand(1, 15);
-                $mult = $_SESSION['mult'];
-                echo '<p>Combien vaut ' . $mult . ' multiplié par ' . $nombre . ' ?</p><form action="test.php" method="post"><input type="text" name="essai" autofocus><input type="submit" value="Envoyer"></form>';
+                echo '<p class="fin">Partie terminée !</br> Votre score :</br> ' . $_SESSION["score"] . ' sur 10</p>';
             }
-            echo '<p>Score : ' . $_SESSION['score'] . ' sur ' . $_SESSION['partie'] . '</p>';
-        } else {
-            echo '<p>Partie terminée ! Votre score : ' . $_SESSION["score"] . ' sur 10</p>';
-        }
-        ?>
-        <div class="bouton">
-            <a href="index.php">Afficher une autre table</a>
-
+            ?>
         </div>
+
     </div>
+
+    <div class="bouton">
+        <a href="index.php">Afficher une autre table</a>
+
+    </div>
+
 
 
     <!-- <script src="js/script.js"></script>   -->
